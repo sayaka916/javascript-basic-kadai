c136cffd2d1b45ae88aedb9de029b4e6486683d6
sayaka916 <sayaka.doglover@gmail.com>
2024-09-21 17:31:16 +0900

    Add kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const textBtn = document.getElementById('btn');
+textBtn.addEventListener('click', () => {
+  const textStr = document.getElementById('text');
+  textStr.textContent="ボタンをクリックしました";
+});
